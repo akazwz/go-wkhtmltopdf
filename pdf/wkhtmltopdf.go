053efe85bdafd4397c852af87fb8b535b5925be5
akez <1791189210@qq.com>
2021-08-24 13:39:51 +0800

    stderr

--- pdf/wkhtmltopdf.go
+++ pdf/wkhtmltopdf.go
@@ -15,10 +15,16 @@ func GeneratePdfFromURL(url string) (err error, filepath string) {
 	var out bytes.Buffer
 	cmd.Stdout = &out
 
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
 	// set stderr to the provided writer, or create a new buffer
 	err = cmd.Run()
 	if err != nil {
 		log.Fatalln("run error", err)
 	}
+
+	log.Println("stderr:", stderr)
+
 	return
 }
